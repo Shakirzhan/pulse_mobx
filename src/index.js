1183e737479382d1a786a184998a7bd32be5511f
Shakirzhan <samarbaev99@mail.ru>
2020-02-20 10:59:54 +0300

    app mobx

--- src/index.js
+++ src/index.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 import DevTools from 'mobx-react-devtools';
+import { observable, computed } from 'mobx';
+import { observer } from 'mobx-react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
+const nickName = observable({
+  firstName: 'Yauhen',
+  age: 30,
+
+  get nickName() {
+    console.log('Generate nickName!');
+    return `${this.firstName}${this.age}`;
+  },
+
+  increment() {
+    this.age++;
+  },
+
+  decrement() {
+    this.age--;
+  }
+});
+
+const todos = observable([{ text: 'Learn React' }, { text: 'Learn MobX' }]);
+
+@observer
+class Counter extends Component {
+  handleIncrement = () => {
+    this.props.store.increment();
+  };
+  handleDecrement = () => {
+    this.props.store.decrement();
+  };
+
   render() {
     return (
       <div className="App">
         <DevTools />
-        <h1>Hello world!</h1>
+        <div class="level_1">
+          <div class="level_2">
+            <span class="child_1">Login</span>
+            <div class="child_2">
+              <TextField
+                label="Username"
+                variant="outlined"
+                id="mui-theme-provider-outlined-input"
+                fullWidth
+                name="username"
+              />
+            </div>
+            <div class="child_3">
+              <Button variant="contained">Login</Button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter store={todos} />, document.getElementById('root'));
+
+todos.push({ text: 'Learn Redux' });
 
 serviceWorker.unregister();
