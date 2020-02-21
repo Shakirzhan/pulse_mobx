7d1d95533f0ce2d6a8f113c8c434f08cf0f6de14
Shakirzhan <samarbaev99@mail.ru>
2020-02-21 08:28:42 +0300

    add axios

--- src/index.js
+++ src/index.js
@@ -7,51 +7,69 @@ import { observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-const nickName = observable({
-  firstName: 'Yauhen',
-  age: 30,
+const store = observable({
+  username: '',
+  password: '',
 
-  get nickName() {
-    console.log('Generate nickName!');
-    return `${this.firstName}${this.age}`;
+  change(name, value) {
+    this[name] = value;
   },
 
-  increment() {
-    this.age++;
-  },
-
-  decrement() {
-    this.age--;
+  submit() {
+    const post = axios.post(`http://localhost:4000/users/authenticate`, 
+    {
+      "username": this.username,
+      "password": this.password
+    });
+    return post.then(res => res);
   }
 });
 
-const todos = observable([{ text: 'Learn React' }, { text: 'Learn MobX' }]);
-
 @observer
 class Counter extends Component {
-  handleIncrement = () => {
-    this.props.store.increment();
-  };
-  handleDecrement = () => {
-    this.props.store.decrement();
-  };
+  onChange(name, value) {
+    this.props.store.change(name, value);
+  }
+
+  onSubmit() {
+    this.props.store.submit().then(res => {console.log(res)});
+  }
 
   render() {
+    const { store: {
+      username,
+      password
+    } } = this.props;
+    console.log(this.props);
     return (
       <div className="App">
         <DevTools />
-        <div class="level_1">
-          <div class="level_2">
-            <span class="child_1">Login</span>
-            <div class="child_2">
-              <TextField label="Username" variant="outlined" fullWidth name="username" />
+        <div className="level_1">
+          <div className="level_2">
+            <span className="child_1">Login</span>
+            <div className="child_2">
+              <TextField
+                label="Username"
+                variant="outlined"
+                fullWidth
+                name="username"
+                value={username}
+                onChange={(e) => this.onChange(e.target.name, e.target.value)} />
             </div>
-            <div class="child_2">
-              <TextField label="Password" variant="outlined" fullWidth name="password" />
+            <div className="child_2">
+              <TextField
+                label="Password"
+                variant="outlined"
+                fullWidth
+                name="password"
+                value={password}
+                type="password"
+                onChange={(e) => this.onChange(e.target.name, e.target.value)} />
             </div>
-            <div class="child_3">
-              <Button variant="contained">Login</Button>
+            <div className="child_3">
+              <Button variant="contained" onClick={() => this.onSubmit()}>Login</Button>
             </div>
           </div>
         </div>
@@ -60,8 +78,6 @@ class Counter extends Component {
   }
 }
 
-ReactDOM.render(<Counter store={todos} />, document.getElementById('root'));
-
-todos.push({ text: 'Learn Redux' });
+ReactDOM.render(<Counter store={store} />, document.getElementById('root'));
 
 serviceWorker.unregister();
